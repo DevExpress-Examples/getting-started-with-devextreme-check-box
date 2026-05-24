a3698a1ecd7f6e998ebc22f2a6c1c6fe8597b2f0
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 10:36:38 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { DxCheckBoxTypes } from 'devextreme-angular/ui/check-box';
 import notify from 'devextreme/ui/notify';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   onValueChanged(e: DxCheckBoxTypes.ValueChangedEvent): void {
