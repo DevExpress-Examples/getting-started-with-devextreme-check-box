c75ed05bd72c6789a6b142303468bb3429c0b414
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 10:37:51 +0300

    Angular - Update to use standalone components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { DxCheckBoxTypes } from 'devextreme-angular/ui/check-box';
 import notify from 'devextreme/ui/notify';
 
+import { DxCheckBoxModule } from 'devextreme-angular/ui/check-box';
+
 @Component({
     selector: 'app-root',
+    imports: [DxCheckBoxModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent {
   onValueChanged(e: DxCheckBoxTypes.ValueChangedEvent): void {
